c4bb1a0ff0ebccb5d59c8295b0d9e7b5da6ce2d6
riaz <riazkhankse@gmail.com>
2025-09-12 13:07:07 +0500

    Change global.css path in billing.jsx

--- react_frontend/src/components/Billing.jsx
+++ react_frontend/src/components/Billing.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import API from "../api";
-import '../../styles/global.css';
+import '../styles/global.css';
 // import 'D:/guesthouse_app_Cleaned/react_frontend/my-react-web-app/src/styles/global.css';
 import axios from "axios";
 
