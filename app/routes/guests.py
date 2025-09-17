8d11763b93b5caaf4d1d18ba5d79d52e82ae773d
riaz <riazkhankse@gmail.com>
2025-09-17 16:17:41 +0500

    updated crud.py

--- app/routes/guests.py
+++ app/routes/guests.py
@@ -19,7 +19,7 @@ def get_client_id(request: Request) -> str:
     return client_id
 
 @router.post("/")
-def create_guest(request: Request, data: dict):
+def create_guest(request: Request, data: dict = Body(...)):
     client_id = get_client_id(request)
     success = crud.create_guest(client_id, data)
     if success:
