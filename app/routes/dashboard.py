9ff8e5e610c97583efeabed5114a18338256412a
riaz <riazkhankse@gmail.com>
2025-09-19 00:23:11 +0500

    updated dashboard.py

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -14,4 +14,4 @@ def get_client_id(request: Request) -> str:
 
 @router.get("/monthly")
 def monthly_dashboard_data(client_id, user: dict = Depends(get_current_user)):
-    return curd_dashboar.get_dashboard_data(client_id)
+    return curd_dashboard.get_dashboard_data(client_id)
