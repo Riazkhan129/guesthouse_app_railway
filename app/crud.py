a7598118cb28ab3dcb1c9d46b60b50de56cdd10e
riaz <riazkhankse@gmail.com>
2025-09-18 22:54:15 +0500

    updated crud.py

--- app/crud.py
+++ app/crud.py
@@ -467,7 +467,7 @@ def checkin_booking(client_id: str, booking_id: int, data):
 
 def get_checkedin_bookings(client_id: str):
     conn, _ = get_or_create_client_db(client_id)  # ✅ UPDATED
-    cursor.cursor()
+    cursor.conn.cursor()
     cursor.execute("SELECT * FROM bookings WHERE status = 'checked_in'")
     rows = cursor.fetchall()
     print("GET_CHECKEDIN_BOOKINGS ----- ROWS = ", rows)
