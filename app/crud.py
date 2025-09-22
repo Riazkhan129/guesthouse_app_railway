a7dfcda60ed153fecf32bc9d4c87476523bb91da
riaz <riazkhankse@gmail.com>
2025-09-22 14:15:36 +0500

    crud.py

--- app/crud.py
+++ app/crud.py
@@ -720,7 +720,7 @@ def create_user(client_id: str, user_data):
 
 #-------
 
-def get_all_users(conn):
+def get_all_users(client_id):
     conn, _ = get_or_create_client_db(client_id)  # ✅ UPDATED
     cursor = conn.cursor()
     cursor.execute("SELECT user_id, name, username, password, role FROM users")
