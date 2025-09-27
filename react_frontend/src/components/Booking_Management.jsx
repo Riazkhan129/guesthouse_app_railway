8a15ac9d87f7e304e2b69d98ce04eb0a527dc458
riaz <riazkhankse@gmail.com>
2025-09-27 16:39:51 +0500

    updated booking.jsx

--- react_frontend/src/components/Booking_Management.jsx
+++ react_frontend/src/components/Booking_Management.jsx
@@ -237,7 +237,7 @@ useEffect(() => {
                   textAlign: "left",                 // ✅ Center text inside each item
               }}
             > */}
-                {/*  <div style={{ minWidth: "100px" }}>
+                  <div style={{ minWidth: "100px", justifyContent: "right", }}>
                     <strong>✅ Total:</strong> {availability.total_rooms}
                   </div>
                   <div style={{ minWidth: "100px" }}>
@@ -246,7 +246,7 @@ useEffect(() => {
                   <div style={{ minWidth: "100px" }}>
                     <strong>🟢 Available:</strong> {availability.available_rooms}
                   </div>
-                 </div> */}
+                 {/*</div> */}
               </div> 
             )} 
               {availability?.available_rooms > 0 ? (
