373b883e3d92aaaa470a0a559116cd35f4569f83
riaz <riazkhankse@gmail.com>
2025-09-28 16:00:07 +0500

    updated booking.jsx

--- react_frontend/src/components/Booking_Management.jsx
+++ react_frontend/src/components/Booking_Management.jsx
@@ -215,7 +215,7 @@ useEffect(() => {
               {availability && (
                 
                 <div style={{ marginTop: 20 }}> 
-                <div style={{ display: "flex", justifyContent: "flex-start"}}>
+                
               {/* <div
                    style={{                   
                     display: "flex",              
