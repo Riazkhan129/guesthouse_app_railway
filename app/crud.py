724d44e14b05cdecfa055c290f50e20cd1b5c986
riaz <riazkhankse@gmail.com>
2025-11-15 13:10:43 +0500

    crud.py

--- app/crud.py
+++ app/crud.py
@@ -181,11 +181,11 @@ def get_all_expense_items(client_id: str):
     ]
 
 def get_expense_items_by_category(client_id: str, category_id: int):
-    conn, _ = get_or_create_client_db(client_id)
+    conn, placeholder = get_or_create_client_db(client_id)
     cursor = conn.cursor()
     print("IN CRUD get_expense_items_by_category", (category_id))
     cursor.execute(
-        "SELECT * FROM expense_items WHERE category_id = ?", (category_id)
+        "SELECT * FROM expense_items WHERE category_id = {placeholder}", (category_id)
     )
     rows = cursor.fetchall()
     print("ROWS = ", rows)
